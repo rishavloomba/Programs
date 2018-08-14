ebd926ab967006e599aad864ae33ec5ccbcd3b0c
shauryaprataap <theshauryapratap@gmail.com>
2018-08-14 12:32:18 +0530

    Second Git

---
+++ TODO.cpp
@@ -0,0 +1,42 @@
+//ToDo
+
+/*Linked list 
+->insertion
+=>at front
+=>at end
+=>at specific position
+->deletion
+=>from front
+=>from end
+=>from specific position
+->sorting
+->searching
+
+
+Double linked list 
+->insertion
+=>at front
+=>at end
+=>at specific position
+->deletion
+=>from front
+=>from end
+=>from specific position
+->sorting
+->searching
+
+Circular Linked list 
+->insertion
+=>at front
+=>at end
+=>at specific position
+->deletion
+=>from front
+=>from end
+=>from specific position
+->sorting
+->searching
+
+Stack
+->infix to postfix 
+->infix to prefix
